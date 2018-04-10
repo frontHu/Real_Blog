18f5b1b80a14675d9fe254f9cc37fdcaba03d1b7
huzhiwei <405163706@qq.com>
2018-04-10 17:14:50 +0800

    first

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import router from "./router/router";
+import { Router, hashHistory } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router routes={router} history={hashHistory} />, 
+  document.getElementById('root')
+);
---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+import App from "./../component/App/App"
+
+const router = [
+  {
+    path: "/",
+    components: App
+  }
+];
+
+export default router; 
