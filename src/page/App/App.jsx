46e29945b28614f4f8fdd80408c1f6734593e9f3
hzw <405163706@qq.com>
2018-12-12 14:59:03 +0800

    修改图片大小，按需加载js有问题

--- src/page/App/App.jsx
+++ src/page/App/App.jsx
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 import "./scss/app.scss";
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import AsyncComponent from './../../untils/AsyncComponent'
-// import IndexPage from './../IndexPage/IndexPage'
-// import BlogPage from './../BlogPage/BlogPage'
-// import Artical from './../Artical/Artical'
-// import Hero from './../Hero/Hero'
-// import AboutMe from './../AboutMe/AboutMe'
-// import Comment from './../Comment/Comment'
-// import Plane from './../Plane/Plane'
+import IndexPage from './../IndexPage/IndexPage'
+import BlogPage from './../BlogPage/BlogPage'
+import Artical from './../Artical/Artical'
+import Hero from './../Hero/Hero'
+import AboutMe from './../AboutMe/AboutMe'
+import Comment from './../Comment/Comment'
+import Plane from './../Plane/Plane'
 
-const IndexPage = AsyncComponent(() => import('./../IndexPage/IndexPage'))
-const BlogPage = AsyncComponent(() => import('./../BlogPage/BlogPage'))
-const Artical = AsyncComponent(() => import('./../Artical/Artical'))
-const Hero = AsyncComponent(() => import('./../Hero/Hero'))
-const AboutMe = AsyncComponent(() => import('./../AboutMe/AboutMe'))
-const Comment = AsyncComponent(() => import('./../Comment/Comment'))
-const Plane = AsyncComponent(() => import('./../Plane/Plane'))
+// const IndexPage = AsyncComponent(() => import('./../IndexPage/IndexPage'))
+// const BlogPage = AsyncComponent(() => import('./../BlogPage/BlogPage'))
+// const Artical = AsyncComponent(() => import('./../Artical/Artical'))
+// const Hero = AsyncComponent(() => import('./../Hero/Hero'))
+// const AboutMe = AsyncComponent(() => import('./../AboutMe/AboutMe'))
+// const Comment = AsyncComponent(() => import('./../Comment/Comment'))
+// const Plane = AsyncComponent(() => import('./../Plane/Plane'))
 
 class App extends Component { 
   render() {
